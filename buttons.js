a797197bc4080b4dcfbb1a6e9697a173613b46c6
Trimblewick <gustav.ekelund@hotmail.com>
2019-03-15 22:52:21 +0100

    Added copy to clipboard button

---
+++ buttons.js
@@ -0,0 +1,26 @@
+function clearSelection()
+{
+ if (window.getSelection) {
+   window.getSelection().removeAllRanges();
+ }
+ else if (document.selection) {
+   document.selection.empty();
+ }
+}
+
+function CopyToClipboard(containerid) {
+if (document.selection) {
+    var range = document.body.createTextRange();
+    range.moveToElementText(document.getElementById(containerid));
+    range.select().createTextRange();
+    document.execCommand("copy");
+
+} else if (window.getSelection) {
+    var range = document.createRange();
+     range.selectNode(document.getElementById(containerid));
+     window.getSelection().addRange(range);
+     document.execCommand("copy");
+
+}
+clearSelection();
+}

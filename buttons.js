548d7c190a3629bc87232b8094a1a9d64cb5460e
Trimblewick <gustav.ekelund@hotmail.com>
2019-03-17 11:32:07 +0100

    maximize and minimize, added icons to buttons

--- buttons.js
+++ buttons.js
@@ -1,26 +1,52 @@
-function clearSelection()
+function ClearSelection()
 {
- if (window.getSelection) {
+ if (window.getSelection)
+ {
    window.getSelection().removeAllRanges();
  }
- else if (document.selection) {
+ else if (document.selection)
+ {
    document.selection.empty();
  }
 }
 
-function CopyToClipboard(containerid) {
-if (document.selection) {
-    var range = document.body.createTextRange();
-    range.moveToElementText(document.getElementById(containerid));
-    range.select().createTextRange();
-    document.execCommand("copy");
+function CopyToClipboard(containerid)
+{
+  if (document.selection)
+  {
+      var range = document.body.createTextRange();
+      var container = document.getElementById(containerid);
+      range.moveToElementText(container);
+      range.select().createTextRange();
+      document.execCommand("copy");
 
-} else if (window.getSelection) {
-    var range = document.createRange();
-     range.selectNode(document.getElementById(containerid));
-     window.getSelection().addRange(range);
-     document.execCommand("copy");
+  }
+  else if (window.getSelection)
+  {
+      var range = document.createRange();
+      var container = document.getElementById(containerid);
+      range.moveToElementText(container);
+      window.getSelection().addRange(range);
+      document.execCommand("copy");
 
+  }
+  ClearSelection();
 }
-clearSelection();
+
+function Collapse(element, containerid)
+{
+  var content = document.getElementById(containerid);
+
+  if (content.style.display === "none")
+  {
+    content.style.display = "block";
+    element.style.backgroundImage = "url(minimize.png)";
+  }
+  else
+  {
+    content.style.display = "none";
+    element.style.backgroundImage = "url(maximize.png)";
+  }
+
+
 }
